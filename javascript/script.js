af9bfeb5969ef8393ba54601ff2c4b02542043b2
kisshor132004 <kisshor2004a@gmail.com>
2024-07-15 15:20:23 +0530

    First Commit

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+var a = document.getElementById("para");
+document.write(a.textContent);
+a.style.backgroundColor = "green"
+
+var a = document.getElementsByClassName("pyh");
+document.write(a.textContent);
